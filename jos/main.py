3caa338bf1858db895b1d305061edd873c3528dd
Thomas Levine <_@thomaslevine.com>
2014-08-01 13:18:47 +0000

    save pdfs

--- jos/main.py
+++ jos/main.py
@@ -1,7 +1,15 @@
+import os
+
 import jos.download as d
 import jos.parse as p
 
 starting_url = 'http://www.degruyter.com/view/j/jos.2014.30.issue-2/issue-files/jos.2014.30.issue-2.xml'
+
+PDF_DIR = os.path.join(os.path.expanduser('~'), '.jos', 'pdfs')
+try:
+    os.makedirs(PDF_DIR)
+except FileExistsError:
+    pass
 
 def main():
     issues = set()
@@ -25,5 +33,10 @@ def main():
                 pdf_response = d.get(pdf,
                     headers = {'referer': response.url},
                     cookies = dict(response.cookies))
-                if not pdf_response.ok:
+                if pdf_response.ok:
+                    filename = pdf_response.headers['content-disposition'].split('=')[1]
+                    with open(os.path.join(PDF_DIR, filename), 'wb') as fp:
+                        fp.write(pdf_response.content)
+                else:
+                    break
                     raise ValueError('Bad PDF response at %s' % pdf_response.url)
